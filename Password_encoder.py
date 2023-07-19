60b507fa81ac110b3f7edc36a81df815e4e92b1c
allyrecalde <139785194+allyrecalde@users.noreply.github.com>
2023-07-18 20:01:48 -0400

    Update Password_encoder.py
    2. Add the decoder function to your cloned partner's repo and push the change to their GitHub repo.

--- Password_encoder.py
+++ Password_encoder.py
@@ -3,20 +3,21 @@ def encode(password):
     for char in password:
         encoded += str(int(char) + 3)
         return encoded
-
+        
+#decoder function from ally
 def decode(password):
         decoded = ''
         for char in password:
             decoded += str(int(char) - 3)
             return decoded
+            
+#newly formatted print menu options from ally
+print('Menu\n')
+print('-------------\n')
+print('1. Encode\n')
+print('2. Decode\n')
+print('3. Quit\n')
 
-
-
-print("Menu\n"
-          "-------------\n"
-          "1. Encode\n"
-          "2. Decode\n"
-          "3. Quit\n")
 option = int(input("Please enter an option "))
 
 if option == 1:
@@ -25,5 +26,5 @@ if option == 1:
         print(" ")
 
 
-
+#seeing if this works
 
